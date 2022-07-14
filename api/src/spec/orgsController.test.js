3198e4a3a09f94a07284350f66fd0997bc358e0f
darius-despain <darius.despain@gmail.com>
2022-07-14 09:05:45 -0600

    added condition to tests not to run if connection fails

--- api/src/spec/orgsController.test.js
+++ api/src/spec/orgsController.test.js
@@ -10,34 +10,46 @@ app.use(express.json());
 jest.useFakeTimers();
 jest.setTimeout(10 *1000);
 
-knex.raw("SELECT VERSION()")
+let connection = false;
+
+const testConnection = async () => {
+  await knex.raw("SELECT VERSION()")
+  .then(() => {
+    connection = true;
+  })
   .catch(err => {
     console.log(err);
     throw err;
   })
   .finally(() => {
     knex.destroy();
+    connection = false
     return;
   })
+}
 
-describe("Orgs endpoint", () => {
-  beforeAll(() => {
-    app.use('/', orgsRoutes);
-  });
-  describe('/orgs', () => {
-    test('GET: should respond with a 200 and a content-type of json', (done) => {
-      request(app)
-        .get('/orgs')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end((err) => {
-          if (err) throw err;
-          done();
-        })
-    })
-  });
+testConnection();
 
-  afterAll(() => {
-    app.listen().close();
-  });
-})
+if(connection) {
+  describe("Orgs endpoint", () => {
+    beforeAll(() => {
+      app.use('/', orgsRoutes);
+    });
+    describe('/orgs', () => {
+      test('GET: should respond with a 200 and a content-type of json', (done) => {
+        request(app)
+          .get('/orgs')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err) => {
+            if (err) throw err;
+            done();
+          })
+      })
+    });
+
+    afterAll(() => {
+      app.listen().close();
+    });
+  })
+}
